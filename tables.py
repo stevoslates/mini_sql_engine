47a07abd0975710754315e557147a87144ff04dc
Steven Slater <stevenslater@MacBook-Air-7.local>
2025-03-28 12:18:38 +0000

    B-tree

---
+++ tables.py
@@ -0,0 +1,23 @@
+"""
+Controls the creation of tables, the indexes and handles the data.
+"""
+from btree import BTree
+
+class Table:
+    def __init__(self, name, rows, index_column=None):
+        self.name = name
+        self.rows = rows
+        self.index_column = index_column
+        self.index = BTree()
+
+        if self.index_column:
+            for i, row in enumerate(rows):
+                key = row[self.index_column]
+                self.index.insert(key, i)  # store row index as value
+
+    def select():
+        pass
+
+    def select_where():
+        pass
+
